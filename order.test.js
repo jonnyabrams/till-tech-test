17480524c591dd47c964681d1a06b8e36306f23e
Jonny Abrams <jonnyabrams@hotmail.com>
2022-04-28 14:01:55 +0100

    Passed first unit test for Order

---
+++ order.test.js
@@ -0,0 +1,11 @@
+const Order = require('./order.js');
+
+describe('Order', () => {
+  beforeEach(() => {
+    order = new Order;
+  });
+
+  it('is an instance of the Order class', () => {
+    expect(order).toBeInstanceOf(Order);
+  });
+});
